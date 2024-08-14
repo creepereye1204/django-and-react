226b617c738e81c4439451682902d04bb9c10d6a
Creeper Eye <iostream1204@gmail.com>
2024-08-14 06:03:46 +0000

    Commit on 2024-08-14 06:03:46

--- api/views.py
+++ api/views.py
@@ -3,7 +3,8 @@ from rest_framework import generics
 
 from .models import Room
 from .serializers import RoomSerializer
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 import psutil
 # Create your views here.
 
@@ -15,9 +16,14 @@ class RoomView(generics.ListAPIView):
     def post(self, request, *args, **kwargs):
         pass
 
+@api_view(['GET'])
 def dashboard(request):
-    if request.method =='GET':
-        cpu = psutil.cpu_percent(interval=1)
-        memory = psutil.virtual_memory().percent
-        hdd = psutil.hdd().usage / psutil.hdd().total * 100
-        return cpu, memory, hdd
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
+    hdd = psutil.disk_usage('/').percent  # 수정된 부분
+
+    return Response({
+        'cpu': cpu,
+        'memory': memory,
+        'hdd': hdd,
+    })
