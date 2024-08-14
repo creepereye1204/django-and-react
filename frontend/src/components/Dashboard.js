e67c2d5cb68dc9e5d0d02511a54bd49e4786da88
Creeper Eye <iostream1204@gmail.com>
2024-08-14 06:42:45 +0000

    Commit on 2024-08-14 06:42:45

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -12,12 +12,13 @@ function Dashboard() {
     hdd: 0,
     mem: 0,
   });
-  const [loading, setLoading] = useState(true); // 로딩 상태 추가
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("https://my-wiki.p-e.kr:20004/api/dashboard"); // 실제 API URL 사용
+        console.log(response.data); // 데이터 확인
         setData({
           cpu: response.data.cpu,
           hdd: response.data.hdd,
@@ -26,7 +27,7 @@ function Dashboard() {
       } catch (error) {
         console.error("Error fetching data", error);
       } finally {
-        setLoading(false); // 데이터 로딩 완료
+        setLoading(false);
       }
     };
 
@@ -50,8 +51,8 @@ function Dashboard() {
     <div className="dashboard">
       <h1>System Dashboard</h1>
       <div className="chart-container">
-        {loading ? ( // 로딩 상태에 따라 조건부 렌더링
-          <p>Loading...</p> // 로딩 중 메시지
+        {loading ? (
+          <p>Loading...</p>
         ) : (
           <Bar
             data={chartData}
