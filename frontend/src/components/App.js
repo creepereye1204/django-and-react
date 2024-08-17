d78a27f5d1527c341792c5718e20374ac8018ca7
Creeper Eye <iostream1204@gmail.com>
2024-08-17 12:33:53 +0000

    Commit on 2024-08-17 12:33:53

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Write from "./Write";
 import NightCity from '../assets/NightCity.mp4';
 
+import About from './About'; // About 컴포넌트 예시
+
+
 class Navbar extends Component {
   render() {
     return (
-      <header class="header">
-        <a href="#" class="logo">My Wiki</a>
-        <nav class="navbar">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Portfolio</a>
+      <header className="header">
+        <a href="#" className="logo">My Wiki</a>
+        <nav className="navbar">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/portfolio">Portfolio</a>
           <a href="https://colab.research.google.com/drive/1dYnkNX4YNkyj20mgWJVb9uoAd_lKvD15?usp=sharing">Service</a>
-          <a href="#">Contact</a>
+          <a href="/contact">Contact</a>
         </nav>
       </header>
     );
@@ -22,17 +26,21 @@ class Navbar extends Component {
 }
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <div class="background">
-        <video src={NightCity} autoPlay loop muted/>
-        < Navbar />
-        <Write/>
-      </div>
+      <Router>
+        <div className="background">
+          <video src={NightCity} autoPlay loop muted />
+          <Navbar />
+          <Write/>
+          <Switch>
+
+            <Route path="/about" component={About} />
+           
+            
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
