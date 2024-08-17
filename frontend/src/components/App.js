7ef097ffd347fda4d9d36be082250e022f91ee66
Creeper Eye <iostream1204@gmail.com>
2024-08-17 11:05:35 +0000

    Commit on 2024-08-17 11:05:35

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import "./App.css";
 import Write from "./Write";
-import Jesus from '../assets/Jesus.mp4';
+import NightCity from '../assets/NightCity.mp4';
 
 class Navbar extends Component {
   render() {
@@ -29,7 +29,7 @@ export default class App extends Component {
   render() {
     return (
       <div class="background">
-        <video src={Jesus} autoPlay loop muted/>
+        <video src={NightCity} autoPlay loop muted/>
         < Navbar />
         <Write/>
       </div>
