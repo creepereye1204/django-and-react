eb89cf0ec652a9a0f139a4816829bf5eae983c6c
Creeper Eye <iostream1204@gmail.com>
2024-08-17 13:01:40 +0000

    Commit on 2024-08-17 13:01:40

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
-import App from "./components/App";
+import React from "react"; // React를 가져옵니다.
+import { render } from "react-dom"; // render를 가져옵니다.
+import App from "./components/App"; // App 컴포넌트를 가져옵니다.
 
-
-const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+const appDiv = document.getElementById("app"); // HTML의 app 요소를 가져옵니다.
+render(<App />, appDiv); // App 컴포넌트를 렌더링합니다.
