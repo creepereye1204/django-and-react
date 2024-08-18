fa2dd5b1500c4a8d0d26bf6a29a560fdad33a5a5
Creeper Eye <iostream1204@gmail.com>
2024-08-18 06:58:06 +0000

    Commit on 2024-08-18 06:58:06

--- frontend/src/components/BoardList.js
+++ frontend/src/components/BoardList.js
@@ -53,6 +53,7 @@ class BoardList extends Component {
               src={board.thumbnail ? board.thumbnail : defaultThumbnail} 
               alt={board.title} 
             />
+            <h1>{board.thumbnail}</h1>
             <Link to={`/board/${board.id}`}>{board.title}</Link> {/* 링크에 id 추가 */}
           </Card>
         ))}
