21928b870bfc38c83d06a70d2b34fa42471684b3
Creeper Eye <iostream1204@gmail.com>
2024-08-18 12:09:50 +0000

    Commit on 2024-08-18 12:09:50

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Routes, Route,Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./components/App"; // 페이지 컴포넌트
 import Update from "./update"; // 페이지 컴포넌트
 import Board from "./board";
@@ -9,12 +9,12 @@ import WriteBoard from "./writeBoard";
 function Main() {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route path="/" element={<App />} />
         <Route path="/board" element={<Board/>} />
         <Route path="/write" element={<WriteBoard/>} />
         <Route path="*" element={<Update/>} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
