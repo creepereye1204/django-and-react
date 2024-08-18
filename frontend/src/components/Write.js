e5a15baa586fd0decea53901df293f39d3ab133c
Creeper Eye <iostream1204@gmail.com>
2024-08-18 08:10:27 +0000

    Commit on 2024-08-18 08:10:27

--- frontend/src/components/Write.js
+++ frontend/src/components/Write.js
@@ -7,29 +7,31 @@ import ImageResize from 'quill-image-resize';
 Quill.register('modules/ImageResize', ImageResize);
 
 const toolbarOptions = [
-  ['bold', 'italic', 'underline', 'strike'],        
-  ['blockquote', 'code-block'],               
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  [{ 'script': 'sub'}, { 'script': 'super' }],      
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          
-  [{ 'direction': 'rtl' }],                         
-  [{ 'size': ['small', false, 'large', 'huge'] }],  
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  [{ 'color': [] }, { 'background': [] }],
-  [{ 'font': [] }],
-  [{ 'align': [] }],
-  ['link', 'image', 'video'],
-  ['clean']                                         
+  // 기존 toolbarOptions...
 ];
 
 const ImageResizeOptions = { parchment: Quill.import('parchment') };
 
 const Write = () => {
   const [content, setContent] = useState('');
-  const [title, setTitle] = useState(''); // 제목 상태 추가
+  const [title, setTitle] = useState('');
   const [thumbnail, setThumbnail] = useState('');
+
   const handleChange = (value) => {
     setContent(value);
+  };
+
+  const handleFileSelect = (e) => {
+    setThumbnail(e.target.files[0]);
+  };
+
+  const handleButtonClick = () => {
+    document.getElementById('file-input').click(); // 숨겨진 파일 입력 클릭
+  };
+
+  const handleRemoveFile = () => {
+    setThumbnail(''); // 선택한 파일 제거
+    document.getElementById('file-input').value = ''; // 파일 입력 초기화
   };
 
   async function handleSave() {
@@ -54,14 +56,14 @@ const Write = () => {
   return (
     <div className="editor">
       <div className="input">
-      <input 
-        className="title-input"
-        type="text" 
-        placeholder="제목을 입력하세요" 
-        value={title} // 제목 상태 값
-        onChange={(e) => setTitle(e.target.value)} // 제목 상태 업데이트
-      />
-      <button onClick={handleSave}>저장하기</button>
+        <input 
+          className="title-input"
+          type="text" 
+          placeholder="제목을 입력하세요" 
+          value={title} 
+          onChange={(e) => setTitle(e.target.value)} 
+        />
+        <button onClick={handleSave}>저장하기</button>
       </div>
       <ReactQuill
         value={content}
@@ -72,11 +74,29 @@ const Write = () => {
         }}
       />
       <input 
-          className='thumbnail-container'
-          type="file" 
-          accept="image/*" 
-          onChange={(e) => setThumbnail(e.target.files[0])} 
-          /> 
+        id="file-input" 
+        type="file" 
+        accept="image/*" 
+        style={{ display: 'none' }} // 파일 입력 숨기기
+        onChange={handleFileSelect} 
+      />
+      <button 
+        className="thumbnail-button" 
+        onClick={handleButtonClick}
+      >
+        썸네일 업로드
+      </button>
+      {thumbnail && (
+        <div>
+          <p>선택된 파일: {thumbnail.name}</p>
+          <button 
+            className="remove-button" 
+            onClick={handleRemoveFile}
+          >
+            파일 제거
+          </button>
+        </div>
+      )}
     </div>
   );
 };
