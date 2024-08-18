3e0a6458b8f5bbbb062b8cdbce62a98e3d000deb
Creeper Eye <iostream1204@gmail.com>
2024-08-18 08:55:05 +0000

    Commit on 2024-08-18 08:55:05

--- api/views.py
+++ api/views.py
@@ -33,7 +33,7 @@ def write(request, *args, **kwargs):
     try:
         title = request.data.get('title')
         content = request.data.get('content')
-        thumbnail = request.Filesp[0].get('thumbnail', None)  # 수정된 부분
+        thumbnail = request.Files[0].get('thumbnail', None)  # 수정된 부분
         if thumbnail:
             Board.objects.create(title=title, content=content, thumbnail=thumbnail)
         else:
