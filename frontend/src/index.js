4f21a8c2f835d74cde75bf37aef0085efd5ceb67
Creeper Eye <iostream1204@gmail.com>
2024-08-18 04:01:44 +0000

    Commit on 2024-08-18 04:01:44

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./components/App"; // 페이지 컴포넌트
-import update from "./update"; // 페이지 컴포넌트
+import Update from "./update"; // 페이지 컴포넌트
 
 function Main() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="*" element={<update/>} />
+        <Route path="*" element={<Update/>} />
       </Routes>
     </Router>
   );
