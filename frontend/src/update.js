5e97142c44bad171023b31c2b5929cd676bf0284
Creeper Eye <iostream1204@gmail.com>
2024-08-19 01:56:44 +0000

    Commit on 2024-08-19 01:56:44

--- frontend/src/update.js
+++ frontend/src/update.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import App from "./components/App";
 import "./css/update.css";
-
+import update from "./assets/update.jpg";
 
 class Update extends Component {
   render() {
     return (
       <div>
+        <img src={update}></img>
         <h1 id="update">Update Page</h1>
         <App/>
         
