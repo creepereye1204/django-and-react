5fb15907692424787df7ad0745eaf13baff3a1c6
Creeper Eye <iostream1204@gmail.com>
2024-08-22 06:50:26 +0000

    Commit on 2024-08-22 06:50:26

--- api/views.py
+++ api/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render
 from rest_framework import generics
-
+from PIL import Image
 from .models import Room, Board
 from .serializers import RoomSerializer,BoardSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import psutil
 import ollama
+from functools import wraps
+from django.http import JsonResponse
+
+
+class IntegrityError(Exception):
+    pass
+
+def check_integrity(thumbnails):
+    if len(thumbnails) != 1:  # thumbnails의 길이를 체크
+        raise IntegrityError("하나의 파일만 선택하세요.")
+
+    
+def check_data(func):
+    @wraps(func)
+    def _wrapped_func(request, *args, **kwargs):
+        thumbnails = request.data.getlist('thumbnail', None)
+        
+        if request.method == 'POST' and thumbnails:
+            try:
+                check_integrity(thumbnails)
+                
+            except IntegrityError as e:  # IntegrityError를 먼저 처리
+                return JsonResponse({"error": f"Invalid data format: {str(e)}"}, status=400)
+            except Exception as e:  # 그 다음에 일반 예외 처리
+                return JsonResponse({"error": f"Invalid image format: {str(e)}"}, status=400)
+
+        return func(request, *args, **kwargs)
+    
+    return _wrapped_func
+
+
+
 
 class Bible():
     def __init__(self,path:str="biblebot/",table:str='blible_counseling'):
@@ -51,6 +83,8 @@ class RoomView(generics.ListAPIView):
     def post(self, request, *args, **kwargs):
         pass
 
+
+
 @api_view(['GET'])
 def dashboard(request):
     cpu = psutil.cpu_percent(interval=1)
@@ -68,7 +102,7 @@ def write(request, *args, **kwargs):
     try:
         title = request.data.get('title')
         content = request.data.get('content')
-        thumbnail = request.data.get('thumbnail',None)
+        thumbnail = request.FILES.get('thumbnail',None)
         if thumbnail:
             Board.objects.create(title=title, content=content, thumbnail=thumbnail)
         else:
