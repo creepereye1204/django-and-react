a33daa6252f4b8c3663a377ceb31d025e710dc0f
Creeper Eye <iostream1204@gmail.com>
2024-08-22 11:10:59 +0000

    Commit on 2024-08-22 11:10:59

--- api/views.py
+++ api/views.py
@@ -7,59 +7,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import psutil
 import ollama
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from django.views import View
-from django.core.files.storage import FileSystemStorage
-from functools import wraps
-
-
-class IntegrityError(Exception):
-    pass
-
-def check_integrity(thumbnails):
-    allowed_mime_types = ['image/jpeg', 'image/png', 'image/gif']  # 허용할 MIME 타입
-    allowed_extensions = ['.jpg', '.jpeg', '.png', '.gif']  # 허용할 파일 확장자
-    max_size = 5 * 1024 * 1024  # 5MB
-    raise IntegrityError(thumbnails[0].name,thumbnails[0].content_type,thumbnails[0].size)
-    # 파일이 없거나 하나만 존재하는 경우만 허용
-    if len(thumbnails) not in [0, 1]:  
-        raise IntegrityError("파일이 없거나 하나의 파일만 선택하세요.")
-
-    if len(thumbnails) == 1:
-        thumbnail = thumbnails[0]
-
-        # 파일 크기 검사
-        if thumbnail.size > max_size:
-            raise IntegrityError("파일 크기가 5MB를 초과할 수 없습니다.")
-        
-        # MIME 타입 검사
-        if thumbnail.content_type not in allowed_mime_types:
-            raise IntegrityError("허용되지 않는 이미지 형식입니다.")
-            
-        # 파일 확장자 검사
-        if not any(thumbnail.name.lower().endswith(ext) for ext in allowed_extensions):
-            raise IntegrityError("허용되지 않는 파일 확장자입니다.")
-        
-        # 이미지의 너비와 높이를 가져오는 함수 (예: Pillow 라이브러리 사용)
-       
-        
-        
-
-def check_data(func):
-    @wraps(func)
-    def _wrapped_func(request, *args, **kwargs):
-        thumbnails = request.FILES.getlist('thumbnail', None)
-        
-        try:
-            check_integrity(thumbnails)
-        except IntegrityError as e:  # IntegrityError를 처리
-            return JsonResponse({"error": f"데이터 무결성 오류: {str(e)}"}, status=400)
-
-        return func(request, *args, **kwargs)
-    
-    return _wrapped_func
 
 class Bible():
     def __init__(self,path:str="biblebot/",table:str='blible_counseling'):
@@ -119,7 +66,6 @@ def dashboard(request):
     })
 
 @api_view(['POST'])
-@check_data
 def write(request, *args, **kwargs):
     try:
         title = request.data.get('title')
