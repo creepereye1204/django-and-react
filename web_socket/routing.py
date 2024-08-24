69182e8eea16c15f2f8df2994a4440d769989cf3
Creeper Eye <iostream1204@gmail.com>
2024-08-24 02:21:39 +0000

    Commit on 2024-08-24 02:21:39

--- web_socket/routing.py
+++ web_socket/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/data', consumers.DataConsumer.as_asgi()),
+    path('data', consumers.DataConsumer.as_asgi()),
 ]
