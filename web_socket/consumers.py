7e94dc64c3b0b1a8374a8180ced9767e29918629
Creeper Eye <iostream1204@gmail.com>
2024-08-24 06:42:13 +0000

    Commit on 2024-08-24 06:42:13

--- web_socket/consumers.py
+++ web_socket/consumers.py
@@ -1,17 +1,16 @@
-# myapp/consumers.py
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class DataConsumer(WebsocketConsumer):
+class DataConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.accept()
+        await self.accept()  # 클라이언트 연결 수락
 
     async def disconnect(self, close_code):
-        pass
+        pass  # 연결 종료 시 추가 처리 로직이 필요할 경우 여기에 작성
 
     async def receive(self, text_data):
-        data = json.loads(text_data)
+        data = json.loads(text_data)  # 클라이언트로부터 받은 데이터 파싱
         # 받은 데이터를 처리하고 클라이언트에게 전송
         await self.send(text_data=json.dumps({
-            'message': data['message']
+            'message': data['message']  # 받은 메시지를 그대로 클라이언트에게 전송
         }))
