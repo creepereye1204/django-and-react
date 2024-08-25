46b4d83c34b8f37e79340ed0570130de62774fb4
Creeper Eye <iostream1204@gmail.com>
2024-08-25 04:26:50 +0000

    Commit on 2024-08-25 04:26:50

--- web_socket/consumers.py
+++ web_socket/consumers.py
@@ -1,22 +1,59 @@
 
 import json
 import asyncio
+import socketio
 import websockets
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+import socketio
+
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+import socketio
+image_path = '/home/apps/my_wiki/web_socket/img.png'  # 여기에 실제 이미지 파일 경로를 입력하세요.
+
+with open(image_path, 'rb') as image_file:
+    image_data = image_file.read()
+# Flask-SocketIO 클라이언트 인스턴스 생성
+import json
+import socketio
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+# Flask-SocketIO 클라이언트 인스턴스 생성
+flask_sio = socketio.Client()
 
 class DataConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()  # 클라이언트 연결 수락
 
+        # Flask 서버에 연결
+        flask_sio.connect('http://localhost:20004')
+
+        # Flask 서버로부터 받은 메시지를 처리하기 위해 이벤트 핸들러 등록
+        flask_sio.on_event('receive_message', self.handle_receive_message)
+
     async def disconnect(self, close_code):
-        pass  # 연결 종료 시 추가 처리 로직이 필요할 경우 여기에 작성
+        flask_sio.disconnect()  # Flask 서버 연결 종료
 
     async def receive(self, text_data):
-        data = json.loads(text_data)  # 클라이언트로부터 받은 데이터 파싱
-        # 받은 데이터를 처리하고 클라이언트에게 전송
+        # 클라이언트로부터 받은 데이터
+        data = json.loads(text_data)
+
+        # Flask 서버에 데이터 전송
+        flask_sio.emit('send_message', data)
+
+    def handle_receive_message(self, message):
+        # Flask 서버로부터 받은 데이터 처리
+        asyncio.run(self.send_to_client(message))
+
+    async def send_to_client(self, message):
+        # 클라이언트에게 메시지 전송
         await self.send(text_data=json.dumps({
-            'message': data['message']  # 받은 메시지를 그대로 클라이언트에게 전송
+            'message': message  # 받은 메시지를 클라이언트에게 전송
         }))
+
+
 
 
 
