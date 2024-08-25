bd9648ed1a5087fb1fb80673f276d69cacc26eb9
Creeper Eye <iostream1204@gmail.com>
2024-08-25 11:37:53 +0000

    Commit on 2024-08-25 11:37:53

--- web_socket/consumers.py
+++ web_socket/consumers.py
@@ -75,7 +75,7 @@ class SketchToImageConsumer(AsyncWebsocketConsumer):
         asyncio.run(self.send_to_client(message))  # 비동기적으로 클라이언트에게 메시지 전송
 
     async def send_to_client(self, message):
-        logging.info(f'Received message from Flask server: {message}')s
+        logging.info(f'Received message from Flask server: {message}')
         await self.send(text_data=json.dumps({
             'message': message  # 받은 메시지를 클라이언트에게 전송
         }))
