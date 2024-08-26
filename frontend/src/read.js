d80046ad8c8646277a103e330dfcb6e624ebe61a
Creeper Eye <iostream1204@gmail.com>
2024-08-26 03:07:33 +0000

    Commit on 2024-08-26 03:07:33

--- frontend/src/read.js
+++ frontend/src/read.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import App from "./components/App";
 import BoardWrapper from "./components/Board";
-import './css/read.css'; // 스타일 파일을 임포트합니다.
-
 class Read extends Component {
   render() {
     return (
-      <div className="container">
-        <App className="app" writeMode={true} />
-        <BoardWrapper className="board" />
+      <div>
+        
+        <App writeMode={true}/>
+        <BoardWrapper/>
       </div>
     );
   }
 }
 
 export default Read;
+
