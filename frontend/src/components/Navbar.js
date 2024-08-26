ad959b5c95a3ac9f0b12df0b9a87873021b93a63
Creeper Eye <iostream1204@gmail.com>
2024-08-26 03:37:43 +0000

    Commit on 2024-08-26 03:37:43

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
   };
 
   return (
-    <header className= "normalHeader">
+    <header className= "header">
       <a href="#" className="logo">My Wiki</a>
       <div className="menu-toggle" onClick={toggleMenu}>
         <div></div>
