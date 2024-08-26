1a5c65223c606207b100e37a66df35be4676e012
Creeper Eye <iostream1204@gmail.com>
2024-08-26 02:53:52 +0000

    Commit on 2024-08-26 02:53:52

--- frontend/src/read.js
+++ frontend/src/read.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import App from "./components/App";
 import BoardWrapper from "./components/Board";
+
 class Read extends Component {
   render() {
     return (
       <div>
-        
-        <App writeMode={true}/>
-        <BoardWrapper/>
+        <div style={{ position: 'relative', zIndex: 10 }}>
+          <App writeMode={true} />
+        </div>
+        <div style={{ position: 'relative', zIndex: 5 }}>
+          <BoardWrapper />
+        </div>
       </div>
     );
   }
 }
 
 export default Read;
-
