a5cd2a0b04a06865521725b3a2a3627a00583f7d
Creeper Eye <iostream1204@gmail.com>
2024-08-26 03:02:07 +0000

    Commit on 2024-08-26 03:02:07

--- frontend/src/read.js
+++ frontend/src/read.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 import App from "./components/App";
 import BoardWrapper from "./components/Board";
+import './css/read.css'; // 스타일 파일을 임포트합니다.
 
 class Read extends Component {
   render() {
     return (
-      <div>
-        <div style={{ zIndex: 10 }}>
-          <App writeMode={true} />
-        </div>
-        <div style={{ zIndex: 5 }}>
-          <BoardWrapper />
-        </div>
+      <div className="container">
+        <App className="app" writeMode={true} />
+        <BoardWrapper className="board" />
       </div>
     );
   }
