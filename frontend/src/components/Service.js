1a63744558961600185e7c2aa5c4785b6e38ba02
Creeper Eye <iostream1204@gmail.com>
2024-09-01 04:01:26 +0000

    Commit on 2024-09-01 04:01:26

--- frontend/src/components/Service.js
+++ frontend/src/components/Service.js
@@ -1,99 +1,99 @@
-import './Service.css';
-import React, { useState, useEffect } from 'react';
+// import './Service.css';
+// import React, { useState, useEffect } from 'react';
 
-const Service = () => {
-  const [chat, setChat] = useState([]);
-  const recognition = new window.webkitSpeechRecognition();
+// const Service = () => {
+//   const [chat, setChat] = useState([]);
+//   const recognition = new window.webkitSpeechRecognition();
 
-  useEffect(() => {
-    recognition.continuous = true;
-    recognition.interimResults = false;
-    recognition.lang = 'ko-KR';
+//   useEffect(() => {
+//     recognition.continuous = true;
+//     recognition.interimResults = false;
+//     recognition.lang = 'ko-KR';
 
-    recognition.onresult = async (event) => {
-      recognition.stop();
-      for (let i = event.resultIndex; i < event.results.length; ++i) {
-        var clientChat;
-        var serverChat;
-        if (event.results[i].isFinal) {
+//     recognition.onresult = async (event) => {
+//       recognition.stop();
+//       for (let i = event.resultIndex; i < event.results.length; ++i) {
+//         var clientChat;
+//         var serverChat;
+//         if (event.results[i].isFinal) {
 
 
-          try{
-            clientChat = event.results[i][0].transcript;
-            addMessage(clientChat);
-          }
-          catch (error){
-            alert(error.message);
-            return
-          }
+//           try{
+//             clientChat = event.results[i][0].transcript;
+//             addMessage(clientChat);
+//           }
+//           catch (error){
+//             alert(error.message);
+//             return
+//           }
           
-          try {
-            serverChat = await sendQuestionToServer(clientChat);
-            addMessage(serverChat);
-          }
-          catch (error) {
-            alert(error.message);
-            addMessage('오류발생!');
-          }
+//           try {
+//             serverChat = await sendQuestionToServer(clientChat);
+//             addMessage(serverChat);
+//           }
+//           catch (error) {
+//             alert(error.message);
+//             addMessage('오류발생!');
+//           }
 
-        }
-      }
-    };
+//         }
+//       }
+//     };
 
-    recognition.onerror = (event) => {
-      console.error("음성 인식 오류: ", event.error);
-      recognition.start(); // 오류 발생 시 음성 인식을 다시 시작
-    };
+//     recognition.onerror = (event) => {
+//       console.error("음성 인식 오류: ", event.error);
+//       recognition.start(); // 오류 발생 시 음성 인식을 다시 시작
+//     };
 
-    recognition.onend = () => {
-      console.log("음성 인식이 종료되었습니다. 다시 시작합니다...");
-      recognition.start(); // 인식이 종료되면 다시 시작
-    };
+//     recognition.onend = () => {
+//       console.log("음성 인식이 종료되었습니다. 다시 시작합니다...");
+//       recognition.start(); // 인식이 종료되면 다시 시작
+//     };
 
-    recognition.start(); // 컴포넌트가 마운트되면 음성 인식 시작
+//     recognition.start(); // 컴포넌트가 마운트되면 음성 인식 시작
 
-    return () => {
-      recognition.stop(); // 컴포넌트 언마운트 시 음성 인식 정지
-    };
-  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때만 실행
+//     return () => {
+//       recognition.stop(); // 컴포넌트 언마운트 시 음성 인식 정지
+//     };
+//   }, []); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때만 실행
 
-  const addMessage = (message) => {
-    setChat((prevChat) => [...prevChat, message]);
-  };
+//   const addMessage = (message) => {
+//     setChat((prevChat) => [...prevChat, message]);
+//   };
 
-  const sendQuestionToServer = async (question) => {
-    try {
-      const response = await fetch(`https://my-wiki.p-e.kr/api/service`, {
-        method: 'POST',
-        headers: {
-          'X-CSRFToken': csrfToken,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ question }),
-      });
-      const data = await response.json();
-      return data.result;
-    } catch (error) {
-      return `일시적인 오류: ${error.message}가 발생하였습니다`;
-    }
-  };
+//   const sendQuestionToServer = async (question) => {
+//     try {
+//       const response = await fetch(`https://my-wiki.p-e.kr/api/service`, {
+//         method: 'POST',
+//         headers: {
+//           'X-CSRFToken': csrfToken,
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ question }),
+//       });
+//       const data = await response.json();
+//       return data.result;
+//     } catch (error) {
+//       return `일시적인 오류: ${error.message}가 발생하였습니다`;
+//     }
+//   };
 
-  return (
-    <div className='chat-container'>
-      <h1 className='title'>심신풀이</h1>
-      {chat.map((message, index) => (
-        index % 2 === 0 ? (
-          <div className='client' key={index}>{message}</div> // 짝수 인덱스
-        ) : (
-          <div className='server' key={index}>{message}</div> // 홀수 인덱스
-        )
-      ))}
+//   return (
+//     <div className='chat-container'>
+//       <h1 className='title'>심신풀이</h1>
+//       {chat.map((message, index) => (
+//         index % 2 === 0 ? (
+//           <div className='client' key={index}>{message}</div> // 짝수 인덱스
+//         ) : (
+//           <div className='server' key={index}>{message}</div> // 홀수 인덱스
+//         )
+//       ))}
        
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default Service;
+// export default Service;
 
 
 // class Message{
