7f0315c43dfd88c258220f31f3014e451fb345a4
Creeper Eye <iostream1204@gmail.com>
2025-04-05 10:59:46 +0000

    sss

--- api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Board, User, Comment
-
-# Register your models here.
-admin.site.register(Board)
-admin.site.register(User)
-admin.site.register(Comment)
